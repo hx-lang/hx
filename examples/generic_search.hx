1637851a53ee4ef81cef36d9c78ba834d49b5266
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-08-22 15:24:23 +0200

    Update syntax

--- examples/generic_search.hx
+++ examples/generic_search.hx
@@ -2,26 +2,26 @@
 // Generic counting with effect handlers
 //
 
-let count : (((i64) -> bool) -> bool) -> i64 {
+let count : ((i64 -> bool) -> bool) -> i64 {
   pred ->
     let sig nondet {
-      branch : (i64) -> bool
+      branch : i64 -> bool
     }; // locally generated signature.
     let hcount : <nondet>bool -> i64 {
       ans                   -> if ans then 1 else 0
-      <branch(_) => resume> -> resume(true) + resume(false)
+      <branch(_) => resume> -> resume true + resume false
     };
     hcount (pred nondet::branch),
 }
 
-let xor : (bool, bool) -> bool {
-  (true, false) -> true,
-  (false, true) -> true,
-  _             -> false,
+let xor : bool -> bool -> bool {
+  true false -> true,
+  false true -> true,
+  _          -> false,
 }
 
-let xor_pred : ((i64) -> bool) -> bool {
-  p -> xor(p(0), xor(p(1), xor(p(2), p(3)))),
+let xor_pred : (i64 -> bool) -> bool {
+  p -> xor (p 0) (xor (p 1) (xor (p 2) (p 3))),
 }
 
 let main : i64 = count xor_pred; // returns 4
